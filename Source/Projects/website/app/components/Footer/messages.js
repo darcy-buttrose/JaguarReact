e39efa8f6dee67487ffe5e26b32d5a6bffe1c89c
John Morton <johnmorton503@gmail.com>
2018-05-01 17:10:26 +0800

    website app clean
    clean app level resources - reset to default

---
+++ Source/Projects/website/app/components/Footer/messages.js
@@ -0,0 +1,13 @@
+/*
+ * Footer Messages
+ *
+ * This contains all the text for the Footer component.
+ */
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  licenseMessage: {
+    id: 'boilerplate.components.Footer.license',
+    defaultMessage: ' Copyright iCentana. All rights reserved.',
+  },
+});
