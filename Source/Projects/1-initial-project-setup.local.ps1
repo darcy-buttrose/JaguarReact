289800bd05b1ed7f023f821e071b282e91048232
Darcy Buttrose <darcy.buttrose@gmail.com>
2018-05-06 13:40:26 +0800

    added local setup file.

---
+++ Source/Projects/1-initial-project-setup.local.ps1
@@ -0,0 +1,14 @@
+$HOSTIP = "localhost"
+
+# update configs with Host Machine IP
+(get-content ".\membership\appsettings.json.orig") | foreach-object {$_ -replace "REPLACE_WITH_HOSTIP", $HOSTIP} | set-content ".\membership\appsettings.Development.json"
+(get-content ".\website\app\config\config.jsone") | foreach-object {$_ -replace "REPLACE_WITH_HOSTIP", $HOSTIP} | set-content ".\website\app\config\config.json"
+
+Set-Location website
+write-output (npm install)
+
+Set-Location ..\membership
+
+(dotnet ef database update -C ApplicationDbContext)
+(dotnet ef database update -C ConfigurationDbContext)
+(dotnet ef database update -C PersistedGrantDbContext)
