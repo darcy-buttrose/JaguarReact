ed42bee8140c57fd893565e1fb751df77cfa108c
Darcy Buttrose <darcy.buttrose@gmail.com>
2018-05-24 15:39:55 +0800

    remove unused api. seperated profile api into its own file.

--- Source/Projects/website/server/api/index.js
+++ Source/Projects/website/server/api/index.js
@@ -1,41 +1,11 @@
 import { Router } from 'express';
 import { version } from '../../package.json';
+import profile from './profile';
 
-let profiles = {
-  admin: {
-    role: ['admin'],
-  },
-  icetana: {
-    role: ['operator'],
-  },
-};
-
-function resolveProfile(user) {
-  const profile = {
-    name: user.username,
-  };
-  return Object.assign({}, profile, profiles[profile.name] || { role: ['other'] });
-}
-
-export default () => {
+export default (options) => {
   const api = Router();
 
-  api.get('/profile', (req, res) => {
-    const profile = resolveProfile(req.user);
-    console.log('get profile', profile);
-    res.send(profile);
-  });
-
-  api.put('/profile', (req, res) => {
-    const inProfile = req.body.profile;
-    const { user } = req;
-    console.log('put profile in', inProfile);
-    const existingProfile = resolveProfile(user);
-    profiles[user.username] = Object.assign({}, existingProfile, inProfile);
-    const outProfile = resolveProfile(user);
-    console.log('put profile out', outProfile);
-    res.send(outProfile);
-  });
+  api.use('/profile', profile(options));
 
   // perhaps expose some API metadata at the root
   api.get('/', (req, res) => {
