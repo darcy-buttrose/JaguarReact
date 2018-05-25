0fc5edbbd58474988e9c176900806f9aea9794bd
Darcy Buttrose <darcy.buttrose@gmail.com>
2018-05-25 09:12:00 +0800

    updated ps files for config changes.

--- Source/Projects/1-initial-project-setup.local.ps1
+++ Source/Projects/1-initial-project-setup.local.ps1
@@ -2,7 +2,7 @@ $HOSTIP = "localhost"
 
 # update configs with Host Machine IP
 (get-content ".\membership\appsettings.json.orig") | foreach-object {$_ -replace "REPLACE_WITH_HOSTIP", $HOSTIP} | set-content ".\membership\appsettings.Development.json"
-(get-content ".\website\app\static\appConfig.orig.json") | foreach-object {$_ -replace "REPLACE_WITH_HOSTIP", $HOSTIP} | set-content ".\website\app\static\appConfig.json"
+(get-content ".\website\server\config.orig.json") | foreach-object {$_ -replace "REPLACE_WITH_HOSTIP", $HOSTIP} | set-content ".\website\server\config.json"
 
 Set-Location website
 write-output (npm install)
