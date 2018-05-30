4f489b7edb98e7a52e83aa352d068d8d7219541a
John Morton <john.morton@icetana.com.au>
2018-05-30 16:47:03 +0800

    Web Sidebar
    Initial implementation of sidebar for operator.

--- Source/Projects/website/app/components/SideBar/index.js
+++ Source/Projects/website/app/components/SideBar/index.js
@@ -1,21 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import NavOperator from 'components/NavOperator';
 
 function SideBar() {
   return (
     <div className="jaguar-sidebar">
-      <Link to="/livewall">
-        <span className="fas fa-th fa-2x"></span>
-      </Link><br />
-      <Link to="/playback">
-        <span className="far fa-play-circle fa-2x"></span>
-      </Link><br />
-      <Link to="/playbackwall">
-        <span className="fab fa-youtube fa-2x"></span>
-      </Link><br />
-      <Link to="/history">
-        <span className="fas fa-history fa-2x"></span>
-      </Link>
+      <NavOperator />
     </div>
   );
 }
