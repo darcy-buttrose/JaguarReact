4c59bbd300ac64ca71a038ba6733453f5060e974
John Morton <john.morton@icetana.com.au>
2018-05-31 09:47:19 +0800

    Sidebar - Navlinks
    Implemented react-router Navlink as the sidebar tabs

--- Source/Projects/website/app/components/NavOperator/index.js
+++ Source/Projects/website/app/components/NavOperator/index.js
@@ -1,40 +1,32 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { FormattedMessage } from 'react-intl';
+import messages from './messages';
 
 function NavOperator() {
   return (
     <div className="sidebar-menu">
       <ul>
-        <li>
-          <NavLink to="/livewall" activeClassName="sidebar-link-active">
-            <span className="fas fa-th fa-2x"></span>
-            <p>LiveWall</p>
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/playback">
-            <span className="far fa-play-circle fa-2x"></span>
-            <p>PlayBack</p>
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/playbackwall">
-            <span className="fab fa-youtube fa-2x"></span>
-            <p>PlayBack Wall</p>
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/history">
-            <span className="fas fa-history fa-2x"></span>
-            <p>History</p>
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/livewall">
-            <span className="fas fa-cogs fa-2x"></span>
-            <p>Config</p>
-          </NavLink>
-        </li>
+        <NavLink to="/livewall" activeClassName="sidebar-link-active">
+          <span className="fas fa-th fa-2x"></span>
+          <FormattedMessage {...messages.liveWall} />
+        </NavLink>
+        <NavLink to="/playback">
+          <span className="far fa-play-circle fa-2x"></span>
+          <FormattedMessage {...messages.playback} />
+        </NavLink>
+        <NavLink to="/playbackwall">
+          <span className="fab fa-youtube fa-2x"></span>
+          <FormattedMessage {...messages.playbackWall} />
+        </NavLink>
+        <NavLink to="/history">
+          <span className="fas fa-history fa-2x"></span>
+          <FormattedMessage {...messages.history} />
+        </NavLink>
+        <NavLink to="/livewall">
+          <span className="fas fa-cogs fa-2x"></span>
+          <FormattedMessage {...messages.config} />
+        </NavLink>
       </ul>
     </div>
   );
