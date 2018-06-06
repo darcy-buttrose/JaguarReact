76c12cc8bb229e2457e7e0ec996899a1fff00be2
John Morton <john.morton@icetana.com.au>
2018-06-06 15:55:01 +0800

    React Reflex Add
    Add react-reflex package

--- Source/Projects/website/app/layouts/MainLayout/index.js
+++ Source/Projects/website/app/layouts/MainLayout/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import SideBar from 'components/SideBar';
 import { ToastContainer } from 'react-toastify';
+import AppHeader from 'containers/AppHeader';
+import StatusBar from 'components/StatusBar';
+
 import 'react-toastify/dist/ReactToastify.min.css';
-import AppHeader from '../../containers/AppHeader';
-import StatusBar from '../../components/StatusBar';
 
 function MainLayout(props) {
   return (
