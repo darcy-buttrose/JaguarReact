11f979e9fef50962acd679993b34081a3314ba46
claudiomastinu <claudio.mastinu@icetana.com.au>
2018-06-13 10:16:11 +0800

    updated as review comments

--- Source/Projects/website/app/components/CameraFilter/index.js
+++ Source/Projects/website/app/components/CameraFilter/index.js
@@ -1,49 +1,45 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 
+import cameraFilterPropTypes from './propTypes';
+
 
 class CameraFilter extends PureComponent {
   render() {
     const currentFilter = this.props.filters.find((f) => f.id === this.props.filter);
     const totalNumOfCameras = this.props.filters.length > 1 ? this.props.filters[1].total_num_cameras : 0;
-    const renderFilter = (el) => (
+    const renderFilterList = (filter) => (
       <li
-        key={el.id}
-        className={(currentFilter === el) ? 'app-camerafilter-menu-selected' : 'app-camerafilter-menu-item'}
+        key={filter.id}
+        className={(currentFilter === filter) ? 'app-camerafilter-menu-selected' : 'app-camerafilter-menu-item'}
         onClick={() => {
-          this.props.onChangeFilter(el.id);
+          this.props.onChangeFilter(filter.id);
         }}
         role="presentation"
       >
-        {el.name} ({el.id === 0 ? totalNumOfCameras : el.view_num_cameras })
+        {filter.name} ({filter.id === 0 ? totalNumOfCameras : filter.view_num_cameras })
       </li>
     );
 
 
-    let cameraFilter = (
+    let filterIconSize = 'fa-2x';
+    let filterMenuClass = 'app-camerafilter-menu';
+    if (this.props.position === 'footer' && !this.props.liveWallFullScreen) {
+      filterIconSize = 'fa-lg';
+      filterMenuClass = 'app-camerafilter-menu-dropup';
+    }
+
+    const cameraFilter = (
       <span>
         <span
-          className="fas fa-filter fa-2x"
+          className={`fas fa-filter ${filterIconSize}`}
           role="presentation"
         />
-        <ul className="app-camerafilter-menu">
-          {this.props.filters.map(renderFilter)}
+        <ul className={filterMenuClass}>
+          {this.props.filters.map(renderFilterList)}
         </ul>
       </span>
     );
-    if (this.props.position === 'footer' && !this.props.liveWallFullScreen) {
-      cameraFilter = (
-        <span>
-          <span
-            className="fas fa-filter fa-lg"
-            role="presentation"
-          />
-          <ul className="app-camerafilter-menu-dropup">
-            {this.props.filters.map(renderFilter)}
-          </ul>
-        </span>
-      );
-    }
 
     return (
       <span className="app-camerafilter">
@@ -56,12 +52,7 @@ class CameraFilter extends PureComponent {
 
 CameraFilter.propTypes = {
   filter: PropTypes.number,
-  filters: PropTypes.arrayOf(PropTypes.shape({
-    id: PropTypes.number,
-    name: PropTypes.string,
-    total_num_cameras: PropTypes.number,
-    view_num_cameras: PropTypes.number,
-  })),
+  filters: PropTypes.arrayOf(PropTypes.shape(cameraFilterPropTypes)),
   onChangeFilter: PropTypes.func,
   position: PropTypes.string,
   liveWallFullScreen: PropTypes.bool,
