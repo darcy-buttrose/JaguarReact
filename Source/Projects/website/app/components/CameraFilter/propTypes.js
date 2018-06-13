3f6ac9bc150214480ea7f661a178ffb935cce147
claudiomastinu <claudio.mastinu@icetana.com.au>
2018-06-13 11:00:03 +0800

    created after review comment

---
+++ Source/Projects/website/app/components/CameraFilter/propTypes.js
@@ -0,0 +1,12 @@
+import PropTypes from 'prop-types';
+
+
+const cameraFilterPropTypes = {
+  id: PropTypes.number,
+  name: PropTypes.string,
+  total_num_cameras: PropTypes.number,
+  view_num_cameras: PropTypes.number,
+};
+
+
+export default cameraFilterPropTypes;
